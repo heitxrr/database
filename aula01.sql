d1d6d2d67e0c2feb81e0eee00fffe0a1c9706c65
heitxrr <heitorrhh@hotmail.com>
2025-02-10 22:59:49 -0300

    inserts

--- aula01.sql
+++ aula01.sql
@@ -1,58 +1,48 @@
 CREATE TABLE pais (
-    id_pais   NUMBER,
+    id_pais   NUMBER PRIMARY KEY,    -- Incluído o PRIMARY KEY diretamente aqui
     nome_pais VARCHAR2(30)
 );
 
-ALTER TABLE pais ADD PRIMARY KEY ( id_pais );
-
 CREATE TABLE estado (
-    id_estado   NUMBER,
+    id_estado   NUMBER PRIMARY KEY,  -- Incluído o PRIMARY KEY diretamente aqui
     nome_estado VARCHAR2(30),
-    id_pais     NUMBER
+    id_pais     NUMBER,
+    CONSTRAINT fk_estado FOREIGN KEY (id_pais)
+        REFERENCES pais (id_pais)
 );
-
-ALTER TABLE estado ADD PRIMARY KEY ( id_estado );
-
-ALTER TABLE estado
-    ADD CONSTRAINT fk_estado FOREIGN KEY ( id_pais )
-        REFERENCES pais ( id_pais );
 
 CREATE TABLE cidade (
-    id_cidade   NUMBER,
+    id_cidade   NUMBER PRIMARY KEY,  -- Incluído o PRIMARY KEY diretamente aqui
     nome_cidade VARCHAR2(40),
-    id_estado   NUMBER
+    id_estado   NUMBER,
+    CONSTRAINT fk_cidade FOREIGN KEY (id_estado)
+        REFERENCES estado (id_estado)
 );
-
-ALTER TABLE cidade ADD PRIMARY KEY ( id_cidade );
-
-ALTER TABLE cidade
-    ADD CONSTRAINT fk_cidade FOREIGN KEY ( id_estado )
-        REFERENCES estado ( id_estado );
 
 CREATE TABLE bairro (
-    id_bairro   NUMBER,
+    id_bairro   NUMBER PRIMARY KEY,  -- Incluído o PRIMARY KEY diretamente aqui
     nome_bairro VARCHAR2(40),
-    id_cidade   NUMBER
+    id_cidade   NUMBER,
+    CONSTRAINT fk_bairro FOREIGN KEY (id_cidade)
+        REFERENCES cidade (id_cidade)
 );
-
-ALTER TABLE cidade ADD PRIMARY KEY ( id_bairro );
-
-ALTER TABLE bairro
-    ADD CONSTRAINT fk_bairro FOREIGN KEY ( id_bairro )
-        REFERENCES cidade ( id_cidade );
 
 CREATE TABLE endereco_cliente (
-    id_endereco NUMBER,
+    id_endereco NUMBER PRIMARY KEY,  -- Incluído o PRIMARY KEY diretamente aqui
     cep         NUMBER,
-    logradouro  VARCHAR2,
+    logradouro  VARCHAR2(100),
     numero      NUMBER,
     referencia  VARCHAR2(30),
-    id_bairro   NUMBER
+    id_bairro   NUMBER,
+    CONSTRAINT fk_bairro_endereco FOREIGN KEY (id_bairro)
+        REFERENCES bairro (id_bairro)
 );
 
-ALTER TABLE esndereco_cliente ADD PRIMARY KEY ( id_endereco );
+-- EXERCÍCIOS - Inserção de dados na tabela 'pais'
+INSERT INTO pais (id_pais, nome_pais) VALUES (1,'Brasil');
+INSERT INTO pais (id_pais, nome_pais) VALUES (2,'Estados Unidos');
+INSERT INTO pais (id_pais, nome_pais) VALUES (3,'Japao');
+INSERT INTO pais (id_pais, nome_pais) VALUES (4,'Canada');
+INSERT INTO pais (id_pais, nome_pais) VALUES (5,'Congo');
 
-ALTER TABLE endereco_cliente ADD constraint fk_bairro;
-
-FOREIGN KEY REFERENCES ENDERECO_CLIENTE ( ID_BAIRRO );
- 
+INSERT INTO estado (id_estado, nome_estado, id_pais, fk_estado) VALUES (1, 'Sao Paulo', 1, 1);
